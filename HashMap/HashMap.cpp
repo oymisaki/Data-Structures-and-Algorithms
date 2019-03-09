b97d0269d0fd3ade026038b22e5bdc7e40de2725
Lei Yang <lei.yang2015@pku.edu.cn>
2019-03-09 17:45:30 +0800

    add Iterator definition in Hashmap

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -12,7 +12,71 @@ public:
     }
 };
 
-int main()
+// Entry
+template<typename K, typename V>
+class Entry
 {
-    return 0;
-}
+    K key;
+    V value;
+};
+
+// 哈希表
+template<typename K, typename V, typename H>
+class HashMap
+{
+public:
+    type def Entry<const K, V> Entry;
+    class Iterator;
+public:
+    HashMap(int capacity = 100);
+    int size() const;
+    bool empty();
+    Iterator find(const K& k);
+    Iterator put(const K& k, const V& v);
+    void erase(const K& k);
+    void erase(const Iterator& iter);
+    Iterator begin();
+    Iterator end();
+protected:
+    typedef list<Entry> Bucket;
+    typedef vector<Bucket> BktArr;
+    // tbd
+private:
+    int n;
+    H hash;
+    BktArr B;
+public:
+    class Iterator
+    {
+        typedef Vector<Bucket>::Iterator bIter;
+        typedef list<Entry>::Interator eIter;
+        public:
+            Iterator(bIter bp, eIter ep): bp(bp), ep(ep){}
+            V& operator*()
+            {
+                return ep->value;
+            }
+            Iterator operator++()
+            {                    
+                try
+                {
+                    if(bp->end() == ep+1)
+                        return Iterator(bp+1, (bp+1)->begin());
+                    else
+                        return Iterator(bp, ep+1);  
+                }
+                catch(const std::exception& e)
+                {
+                    return Iterator(bp+1, ep);
+                }
+                
+            }
+            bool operator==(const Iterator* iter)
+            {
+                return this->bp == iter->bp && this->ep == iter->ep;
+            }
+        private:
+            bIter bp;
+            eIter ep;
+    }
+};
